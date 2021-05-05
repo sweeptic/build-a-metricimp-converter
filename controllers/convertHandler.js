fe011085060335323fb69f2d09dad8e082f076c8
Horvath Attila <59293104+sweeptic@users.noreply.github.com>
2021-05-05 13:02:51 +0200

    read a fractional input

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -2,13 +2,32 @@ function ConvertHandler() {
   this.getNum = function (input) {
     let result;
     // const regex = /^\d+/;
-    const regex = /^\d+(\.\d+)?/;
+
+    const regex = /^\d+(\.\d+)|^\d+(\/\d+)?/;
 
     let matches = input.match(regex);
 
-    // console.log(matches);
+    // result = matches === null ? null : matches[0];
 
-    result = matches === null ? null : +matches[0];
+    //if any matches
+    if (matches !== null) {
+      const match = matches[0];
+
+      //analyze matched unit
+      //if fractional unit
+      if (match.includes('/')) {
+        result =
+          match.slice(0, match.indexOf('/')) /
+          match.slice(match.indexOf('/') + 1, match.length);
+      } else {
+        //
+        //else is whole or decimal number
+        result = +matches[0];
+      }
+    } else {
+      //if does not matches any
+      result = null;
+    }
 
     return result;
   };
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,36 +6,49 @@ let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function () {
   test('convertHandler should correctly read a whole number input. ', () => {
-    assert.equal(convertHandler.getNum('1'), 1);
-    assert.equal(convertHandler.getNum('12gal'), 12);
-    assert.equal(convertHandler.getNum('123lbs'), 123);
-    assert.equal(convertHandler.getNum('123lbs123'), 123);
-    assert.equal(convertHandler.getNum('123mi'), 123);
+    assert.strictEqual(convertHandler.getNum('1'), 1);
+    assert.strictEqual(convertHandler.getNum('12gal'), 12);
+    assert.strictEqual(convertHandler.getNum('123lbs'), 123);
+    assert.strictEqual(convertHandler.getNum('123lbs123'), 123);
+    assert.strictEqual(convertHandler.getNum('123mi'), 123);
     assert.isNull(convertHandler.getNum('mi123'));
     assert.isNull(convertHandler.getNum('mi123mi'));
     assert.isNull(convertHandler.getNum('   '));
     assert.isNull(convertHandler.getNum('  23mi'));
     assert.isNull(convertHandler.getNum('  mi23'));
   });
+
   test('convertHandler should correctly read a decimal number input. ', () => {
-    assert.equal(convertHandler.getNum('1.1'), 1.1);
-    assert.equal(convertHandler.getNum('1.1111'), 1.1111);
-    assert.equal(convertHandler.getNum('12.6gal'), 12.6);
-    assert.equal(convertHandler.getNum('123.44lbs'), 123.44);
-    assert.equal(convertHandler.getNum('123.555lbs123'), 123.555);
-    assert.equal(convertHandler.getNum('123.1110mi'), 123.111);
+    assert.strictEqual(convertHandler.getNum('1.1'), 1.1);
+    assert.strictEqual(convertHandler.getNum('1.1111'), 1.1111);
+    assert.strictEqual(convertHandler.getNum('12.6gal'), 12.6);
+    assert.strictEqual(convertHandler.getNum('123.44lbs'), 123.44);
+    assert.strictEqual(convertHandler.getNum('123.555lbs123'), 123.555);
+    assert.strictEqual(convertHandler.getNum('123.1110mi'), 123.111);
     assert.isNull(convertHandler.getNum('mi123.34'));
     assert.isNull(convertHandler.getNum('mi123.4mi'));
     assert.isNull(convertHandler.getNum('   '));
     assert.isNull(convertHandler.getNum('  23.2mi'));
     assert.isNull(convertHandler.getNum('  mi23.5'));
   });
+
+  test('convertHandler should correctly read a fractional input. ', () => {
+    assert.strictEqual(convertHandler.getNum('1/2'), 0.5);
+    assert.strictEqual(convertHandler.getNum('12/5gal'), 2.4);
+    assert.strictEqual(convertHandler.getNum('1/2lbs'), 0.5);
+    assert.strictEqual(convertHandler.getNum('1/2lbs123'), 0.5);
+    assert.strictEqual(convertHandler.getNum('1/4mi'), 0.25);
+    assert.isNull(convertHandler.getNum('mi123/34'));
+    assert.isNull(convertHandler.getNum('mi123/3mi'));
+    assert.isNull(convertHandler.getNum('  2/3mi'));
+    assert.isNull(convertHandler.getNum('  mi23/2'));
+  });
 });
 
 /*
 
 
-convertHandler should correctly read a fractional input.
+
 convertHandler should correctly read a fractional input with a decimal.
 convertHandler should correctly return an error on a double-fraction (i.e. 3/2/3).
 convertHandler should correctly default to a numerical input of 1 when no numerical input is provided.
